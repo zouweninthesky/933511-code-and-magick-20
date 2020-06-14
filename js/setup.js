685c93222c85dfef2c221e72998c7495eb430670
Zouwen <zouweninthesky@yandex.ru>
2020-06-14 22:23:17 +0300

    Добавляет открытие/закрытие окна, валидацию имени и смену цветов волшебника

--- js/setup.js
+++ js/setup.js
@@ -4,15 +4,24 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARD_NUMBER = 4;
 
 var userDialog = document.querySelector('.setup');
+var userDialogOpen = document.querySelector('.setup-open');
+var userDialogClose = userDialog.querySelector('.setup-close');
+var userNameInput = userDialog.querySelector('.setup-user-name');
+var userCoat = userDialog.querySelector('.wizard-coat');
+var userEyes = userDialog.querySelector('.wizard-eyes');
+var userFireball = userDialog.querySelector('.setup-fireball');
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var getRandomInt = function (min, max) {
   return min + Math.floor((max - min) * Math.random());
 };
+
+// Функции создания волшебников и данных для них
 
 var createName = function (names, surnames) {
   var flip = getRandomInt(0, 2);
@@ -45,7 +54,6 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-
 var postWizards = function (wizards) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < wizards.length; i++) {
@@ -54,8 +62,94 @@ var postWizards = function (wizards) {
   similarListElement.appendChild(fragment);
 };
 
-userDialog.classList.remove('hidden');
+// Функции вызова меню кастомизации
+
+var onUserDialogEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeUserDialog();
+  }
+};
+
+var openUserDialog = function () {
+  userDialog.classList.remove('hidden');
+
+  document.addEventListener('keydown', onUserDialogEscPress);
+};
+
+var closeUserDialog = function () {
+  userDialog.classList.add('hidden');
+
+  document.removeEventListener('keydown', onUserDialogEscPress);
+};
+
+userDialogOpen.addEventListener('click', function () {
+  openUserDialog();
+});
+
+userDialogOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openUserDialog();
+  }
+});
+
+userDialogClose.addEventListener('click', function () {
+  closeUserDialog();
+});
+
+userDialogClose.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    closeUserDialog();
+  }
+});
+
+// Функции, описывающие меню кастомизации
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function () {
+  var valueLength = userNameInput.value.length;
+
+  if (valueLength < userNameInput.minLength) {
+    userNameInput.setCustomValidity('Ещё ' + (userNameInput.minLength - valueLength) + ' симв.');
+  } else if (valueLength > userNameInput.maxLength) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - userNameInput.maxLength) + ' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+var changeColor = function (array, element, inputName) {
+  var color = array[getRandomInt(0, array.length)];
+  if (element === userFireball) {
+    element.style.backgroundColor = color;
+  } else {
+    element.style.fill = color;
+  }
+  userDialog.querySelector("input[name='" + inputName + "']").value = color;
+};
+
+userCoat.addEventListener('click', function () {
+  changeColor(WIZARD_COATS, userCoat, 'coat-color');
+});
+
+userEyes.addEventListener('click', function () {
+  changeColor(WIZARD_EYES, userEyes, 'eyes-color');
+});
+
+userFireball.addEventListener('click', function () {
+  changeColor(WIZARD_FIREBALL, userFireball, 'fireball-color');
+});
+
+// Тело программы
 
 postWizards(createWizards());
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
